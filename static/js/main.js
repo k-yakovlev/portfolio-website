a0030e529a1b254056b0df6814130f1d5d0c669c
hoolee tester <hooleetest@gmail.com>
2021-04-15 11:05:16 +0300

    Fix type speed in 'Hero' section.

--- static/js/main.js
+++ static/js/main.js
@@ -135,8 +135,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 70,
+      backSpeed: 35,
       backDelay: 2000
     });
   }
