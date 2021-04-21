eb67b090373e2bfccda13b317500413a3d616e81
hoolee tester <hooleetest@gmail.com>
2021-04-21 22:14:10 +0300

    Add current_year in main.py & footer.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
+
+current_year = datetime.date.today().year
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_year=current_year)
 
 
 if __name__ == '__main__':
